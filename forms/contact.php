192f01c929e425e57e5d2245dc3e5322a9753611
Chukwumaobi Precious Nguma <chukwumaobinguma@gmail.com>
2023-03-23 10:16:10 +0100

    update

--- forms/contact.php
+++ forms/contact.php
@@ -1,7 +1,7 @@
 <?php
   $receiving_email_address = 'emilyjones@gmail.com';
 
-  if( file_exists($php_email_form = 'assets/vendor/php-email-form/php-email-form.php' )) {
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
   } else {
     die( 'Unable to load the "PHP Email Form" Library!');
@@ -10,7 +10,7 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = "bdrip1919@gmail.com";
+  $contact->to = "emilyjones@gmail.com";
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
